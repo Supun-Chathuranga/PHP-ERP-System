df49ed5a6d7ce4fdca860ed457da96ca2bb1d603
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-30 22:37:48 +0530

    Update invoice_item_report.php

--- invoice_item_report.php
+++ invoice_item_report.php
@@ -6,13 +6,30 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h2>Invoice Item Report</h2>
-    <form action="process_invoice_item_report.php" method="POST">
-        <label>Start Date:</label>
-        <input type="date" name="start_date" required><br>
-        <label>End Date:</label>
-        <input type="date" name="end_date" required><br>
-        <input type="submit" value="Generate Report">
-    </form>
+    <div class="container">
+        <?php include "sidebar.php"; ?>
+        <div class="content">
+            <h2>Invoice Item Report</h2>
+            <form action="process_invoice_item_report.php" method="POST">
+                <div class="form-row">
+                    <div class="form-column">
+                        <label>Start Date:</label>
+                        <input type="date" name="start_date" required>
+                    </div>
+                    <div class="form-column">
+                        <label>End Date:</label>
+                        <input type="date" name="end_date" required>
+                    </div>
+                    <div class="form-column">
+                        <label>Search Item:</label>
+                        <input type="text" name="search_item" placeholder="Item name or code">
+                    </div>
+                    <div class="form-column">
+                        <input type="submit" value="Generate Report">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
