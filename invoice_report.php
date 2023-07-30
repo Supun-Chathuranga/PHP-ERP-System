3bf7b6878682a0366e239274145e36d78ea1519f
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-30 22:28:27 +0530

    Update invoice_report.php

--- invoice_report.php
+++ invoice_report.php
@@ -6,13 +6,31 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h2>Invoice Report</h2>
-    <form action="process_invoice_report.php" method="POST">
-        <label>Start Date:</label>
-        <input type="date" name="start_date" required><br>
-        <label>End Date:</label>
-        <input type="date" name="end_date" required><br>
-        <input type="submit" value="Generate Report">
-    </form>
+    <div class="container">
+        <?php include "sidebar.php"; ?>
+        <div class="content">
+            <h2>Invoice Report</h2>
+            <form action="process_invoice_report.php" method="POST">
+                <div class="form-row">
+                    <div class="form-column">
+                        <label>Start Date:</label>
+                        <input type="date" name="start_date" required>
+                    </div>
+                    <div class="form-column">
+                        <label>End Date:</label>
+                        <input type="date" name="end_date" required>
+                    </div>
+                    <div class="form-column">
+                        <label>Search Keyword:</label>
+                        <input type="text" name="search_keyword" placeholder="Keyword">
+                    </div>
+                    <div class="form-column">
+                        <input type="submit" value="Generate Report">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
+
