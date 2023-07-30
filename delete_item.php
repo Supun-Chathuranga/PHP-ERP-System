2f2002905f79c31380c853c875ad7cf8083e891e
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-30 21:32:25 +0530

    Create delete_item.php

---
+++ delete_item.php
@@ -0,0 +1,24 @@
+<?php
+require_once "includes/db_config.php";
+
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+    
+    // Delete the item from the database
+    $sql = "DELETE FROM items WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    
+    if ($stmt->execute()) {
+        // Item deleted successfully, redirect back to item_list.php
+        header("Location: item_list.php");
+        exit();
+    } else {
+        // Handle delete error
+    }
+
+    $stmt->close();
+}
+?>
+
+<!-- The rest of the delete_item.php page remains unchanged as in the previous response -->
