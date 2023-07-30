dd671b0929addb4d4f518b7821edd12d9f62b4fc
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-30 19:16:34 +0530

    Update add_item.php

--- add_item.php
+++ add_item.php
@@ -1,4 +1,3 @@
-<!-- add_item.php -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,49 +5,32 @@
     <link rel="stylesheet" href="css/style.css">
     <script>
         function validateForm() {
-            var itemCode = document.forms["itemForm"]["item_code"].value;
-            var itemName = document.forms["itemForm"]["item_name"].value;
-            var quantity = document.forms["itemForm"]["quantity"].value;
-            var unitPrice = document.forms["itemForm"]["unit_price"].value;
-
-            if (itemCode === "") {
-                alert("Please enter the item code.");
-                return false;
-            }
-
-            if (itemName === "") {
-                alert("Please enter the item name.");
-                return false;
-            }
-
-            if (isNaN(quantity) || parseInt(quantity) <= 0) {
-                alert("Please enter a valid quantity.");
-                return false;
-            }
-
-            if (isNaN(unitPrice) || parseFloat(unitPrice) <= 0) {
-                alert("Please enter a valid unit price.");
-                return false;
-            }
+            // Validation logic goes here...
         }
     </script>
 </head>
 <body>
-    <h2>Add Item</h2>
-    <form name="itemForm" action="process_item.php" method="POST" onsubmit="return validateForm()">
-        <label>Item Code:</label>
-        <input type="text" name="item_code" required><br>
-        <label>Item Name:</label>
-        <input type="text" name="item_name" required><br>
-        <label>Item Category:</label>
-        <input type="text" name="item_category"><br>
-        <label>Item Sub Category:</label>
-        <input type="text" name="item_subcategory"><br>
-        <label>Quantity:</label>
-        <input type="number" name="quantity" min="1" required><br>
-        <label>Unit Price:</label>
-        <input type="number" step="0.01" name="unit_price" min="0" required><br>
-        <input type="submit" value="Save">
-    </form>
+    <div class="container">
+        <?php include "sidebar.php"; ?>
+        <div class="content">
+            <h2>Add Item</h2>
+            <form name="itemForm" action="process_item.php" method="POST" onsubmit="return validateForm()">
+                <!-- Your Add Item form content goes here -->
+                <label>Item Code:</label>
+                <input type="text" name="item_code" required><br>
+                <label>Item Name:</label>
+                <input type="text" name="item_name" required><br>
+                <label>Item Category:</label>
+                <input type="text" name="item_category"><br>
+                <label>Item Sub Category:</label>
+                <input type="text" name="item_subcategory"><br>
+                <label>Quantity:</label>
+                <input type="number" name="quantity" min="1" required><br>
+                <label>Unit Price:</label>
+                <input type="number" step="0.01" name="unit_price" min="0" required><br>
+                <input type="submit" value="Save">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
