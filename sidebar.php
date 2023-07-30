247fbf5f998009019508427a8594a0b045ac3515
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-30 16:03:50 +0530

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,22 @@
+<!-- sidebar.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ERP System Sidebar</title>
+    <link rel="stylesheet" href="css/style.css"> <!-- Link to the CSS file -->
+</head>
+<body>
+    <nav class="sidebar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="add_customer.php">Add Customer</a></li>
+            <li><a href="customer_list.php">Customer List</a></li>
+            <li><a href="add_item.php">Add Item</a></li>
+            <li><a href="item_list.php">Item List</a></li>
+            <li><a href="invoice_report.php">Invoice Report</a></li>
+            <li><a href="invoice_item_report.php">Invoice Item Report</a></li>
+            <!-- Add more navigation links as needed for other pages -->
+        </ul>
+    </nav>
+</body>
+</html>
