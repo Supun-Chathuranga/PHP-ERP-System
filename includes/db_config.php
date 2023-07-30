2a87c1af97d68e369fb87ae69a0605cdea9863ba
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-30 21:58:17 +0530

    Create db_config.php

---
+++ includes/db_config.php
@@ -0,0 +1,12 @@
+<?php
+// db_config.php
+$host = "localhost";
+$username = "root";
+$password = "";
+$db_name = "php_erp_system";
+
+$conn = new mysqli($host, $username, $password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
