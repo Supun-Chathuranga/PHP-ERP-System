bbd5f53e853a10ebc0646daaf746f8da3ec69922
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-30 19:16:16 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ERP System</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php include "sidebar.php"; ?>
+    <div class="content">
+        <h1>Welcome to ERP System</h1>
+    </div>
+</body>
+</html>
