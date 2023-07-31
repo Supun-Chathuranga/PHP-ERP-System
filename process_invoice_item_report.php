08e0dc75a6c134f231f455890adbf3c610fa27c5
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-07-31 19:46:13 +0530

    Update process_invoice_item_report.php

--- process_invoice_item_report.php
+++ process_invoice_item_report.php
@@ -3,11 +3,12 @@
 require_once "includes/db_config.php";
 
 $searchKeyword = "";
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Sanitize and validate form data (implement form validation here)
     $startDate = $_POST["start_date"];
     $endDate = $_POST["end_date"];
-    $searchKeyword = $_POST["search_keyword"];
+    $searchKeyword = $_POST["search_keyword"] ?? '';
 
     // Perform server-side validation
     if (empty($startDate) || empty($endDate)) {
@@ -21,17 +22,22 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $endDate = date("Y-m-d", strtotime($endDate));
 
     // Prepare and execute the SQL query to retrieve the invoice item report data
-    $sql = "SELECT invoices.invoice_number, invoices.invoice_date, 
-            CONCAT(customers.first_name, ' ', customers.last_name) AS customer_name,
-            items.item_name, items.item_code, items.item_category, items.unit_price
-            FROM invoices
-            INNER JOIN customers ON invoices.customer_id = customers.id
-            INNER JOIN items ON invoices.item_id = items.id
-            WHERE invoice_date BETWEEN ? AND ?
-            AND (invoices.invoice_number LIKE ? OR customers.first_name LIKE ? OR customers.last_name LIKE ?)";
+    $sql = "SELECT invoice.invoice_no, invoice.date AS invoice_date, customer.first_name, customer.last_name, 
+               item.item_name, item.item_code, item.item_category, invoice_master.unit_price
+            FROM invoice
+            JOIN invoice_master ON invoice.invoice_no = invoice_master.invoice_no
+            JOIN item ON invoice_master.item_id = item.id
+            JOIN customer ON invoice.customer = customer.id
+            WHERE invoice.date BETWEEN ? AND ?
+            AND (invoice.invoice_no LIKE ? OR item.item_name LIKE ?)
+            ORDER BY invoice.date";
+
     $stmt = $conn->prepare($sql);
-    $searchKeyword = "%$searchKeyword%"; // Add wildcards for searching
-    $stmt->bind_param("sss", $startDate, $endDate, $searchKeyword, $searchKeyword, $searchKeyword);
+
+    // Add wildcards for searching
+    $searchKeyword = "%$searchKeyword%";
+    // Adjust the type definition string to match the number of placeholders (4 placeholders here)
+    $stmt->bind_param("ssss", $startDate, $endDate, $searchKeyword, $searchKeyword);
     $stmt->execute();
     $result = $stmt->get_result();
 
@@ -41,6 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 }
 ?>
 
+<!-- HTML Form -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -48,39 +55,54 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h2>Invoice Item Report</h2>
-    <form action="process_invoice_item_report.php" method="POST">
-        <label>Start Date:</label>
-        <input type="date" name="start_date" required><br>
-        <label>End Date:</label>
-        <input type="date" name="end_date" required><br>
-        <label>Search Keyword:</label>
-        <input type="text" name="search_keyword" value="<?php echo htmlspecialchars($searchKeyword); ?>"><br>
-        <input type="submit" value="Generate Report">
-    </form>
+    <div class="container">
+        <?php include "sidebar.php"; ?>
+        <div class="content">
+            <h2>Invoice Report</h2>
+            <form action="process_invoice_report.php" method="POST">
+                <div class="form-row">
+                    <div class="form-column">
+                        <label>Start Date:</label>
+                        <input type="date" name="start_date" required>
+                    </div>
+                    <div class="form-column">
+                        <label>End Date:</label>
+                        <input type="date" name="end_date" required>
+                    </div>
+                    <div class="form-column">
+                        <label>Search Keyword:</label>
+                        <input type="text" name="search_keyword" placeholder="Keyword">
+                    </div>
+                    <div class="form-column">
+                        <input type="submit" value="Generate Report">
+                    </div>
+                </div>
+            </form>
 
-    <h3>Invoice Item Report Results</h3>
-    <table>
-        <tr>
-            <th>Invoice Number</th>
-            <th>Invoiced Date</th>
-            <th>Customer Name</th>
-            <th>Item Name</th>
-            <th>Item Code</th>
-            <th>Item Category</th>
-            <th>Item Unit Price</th>
-        </tr>
-        <?php while ($row = $result->fetch_assoc()) : ?>
-        <tr>
-            <td><?php echo $row["invoice_number"]; ?></td>
-            <td><?php echo $row["invoice_date"]; ?></td>
-            <td><?php echo $row["customer_name"]; ?></td>
-            <td><?php echo $row["item_name"]; ?></td>
-            <td><?php echo $row["item_code"]; ?></td>
-            <td><?php echo $row["item_category"]; ?></td>
-            <td><?php echo $row["unit_price"]; ?></td>
-        </tr>
-        <?php endwhile; ?>
-    </table>
+            <h3>Invoice Item Report Results</h3>
+            <table>
+                <tr>
+                    <th>Invoice Number</th>
+                    <th>Invoiced Date</th>
+                    <th>Customer Name</th>
+                    <th>Item Name</th>
+                    <th>Item Code</th>
+                    <th>Item Category</th>
+                    <th>Item Unit Price</th>
+                </tr>
+                <?php while ($row = $result->fetch_assoc()) : ?>
+                <tr>
+                    <td><?php echo $row["invoice_no"]; ?></td>
+                    <td><?php echo $row["invoice_date"]; ?></td>
+                    <td><?php echo $row["first_name"] . " " . $row["last_name"]; ?></td>
+                    <td><?php echo $row["item_name"]; ?></td>
+                    <td><?php echo $row["item_code"]; ?></td>
+                    <td><?php echo $row["item_category"]; ?></td>
+                    <td><?php echo $row["unit_price"]; ?></td>
+                </tr>
+                <?php endwhile; ?>
+            </table>
+        </div>
+    </div>
 </body>
 </html>
